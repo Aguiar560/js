88ab6fe0fc9b01bcdd11416ae724169becdb56ee
Rafael Aguiar <aguiar560@hotmail.com>
2025-02-05 14:45:12 -0300

    a

---
+++ Site/Calculadora/calculadora.js
@@ -0,0 +1,16 @@
+let display = document.getElementById('display')
+function adicionar(input){
+    display.value += input
+}
+
+function calcular(){                            
+    try{                                               
+        display.value = eval(display.value)              //codigo que pode haver erro
+    }catch(error){
+         display.value = "Error"                        //caso haja erro executa essa linha
+    }
+}
+function limpar(){
+    display.value = ''
+
+}
